d41dd62e4bbb14c77969a481682437dc6a91700d
Rahul Kumar <droid.dev7867@gmail.com>
2026-05-29 17:17:02 +0530

    Added is-Active url.

--- src/main/java/com/raiTech/repository/Categoryrepository.java
+++ src/main/java/com/raiTech/repository/Categoryrepository.java
@@ -1,11 +1,14 @@
 package com.raiTech.repository;
 
+import com.raiTech.dto.CategoryDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.raiTech.entity.Category;
 
-public interface Categoryrepository extends JpaRepository<Category,Integer>{
-	
-	
+import java.util.List;
 
+public interface Categoryrepository extends JpaRepository<Category,Integer>{
+
+
+    List<Category> findByIsActiveTrue();
 }
--- src/main/java/com/raiTech/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/raiTech/service/impl/CategoryServiceImpl.java
@@ -1,8 +1,12 @@
 package com.raiTech.service.impl;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import com.raiTech.dto.CategoryDto;
+import com.raiTech.dto.CategoryResponse;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -16,12 +20,22 @@ public class CategoryServiceImpl implements CategoryService {
 	@Autowired
 	private Categoryrepository categoryRepo;
 
+    @Autowired
+    private ModelMapper mapper;
+
 	@Override
-	public Boolean saveCategory(Category category) {
-		category.setIsDeleted(false);
-        category.setCreatedBy(1);
-        category.setCreatedOn(new Date());
-		Category saveCategory=categoryRepo.save(category);
+	public Boolean saveCategory(CategoryDto categorydto) {
+
+
+//       Category category=new Category();
+//       category.setName(categorydto.getName());
+//       category.setDescription(categorydto.getDescription());
+//       category.setIsActive(categorydto.getIsActive());
+        Category category=mapper.map(categorydto, Category.class);
+       category.setIsDeleted(false);
+       category.setCreatedBy(1);
+       category.setCreatedOn(new Date());
+        Category saveCategory=categoryRepo.save(category);
 		if(ObjectUtils.isEmpty(saveCategory)) {
 			return false;
 		}
@@ -33,5 +47,13 @@ public class CategoryServiceImpl implements CategoryService {
 		List<Category> categories=categoryRepo.findAll();
 		return categories;
 	}
+
+    @Override
+    public List<CategoryResponse> getActiveCategory() {
+        List<Category>categories=categoryRepo.findByIsActiveTrue();
+        List<CategoryResponse>categoryList=categories.stream().map(cat->mapper.map(cat, CategoryResponse.class)).toList();
+
+        return categoryList;
+    }
 
 }
